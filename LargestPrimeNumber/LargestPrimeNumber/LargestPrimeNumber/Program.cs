a733df5cd5fedef10e4ced07691b5f9e2e27a79b
RichardHudsonCodes <rj_hudson@hotmail.co.uk>
2022-05-11 15:35:35 +0100

    first commit

---
+++ LargestPrimeNumber/LargestPrimeNumber/LargestPrimeNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LargestPrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
